512e62da40101108a4c33319335a3aada38ac247
Satish Shingrakhiya <satish.shingrakhiy@gmail.com>
2025-07-10 11:15:06 +0530

    Implement Math Problem 1

--- src/main/java/com/javapractice/java/App.java
+++ src/main/java/com/javapractice/java/App.java
@@ -1,10 +1,16 @@
 package com.javapractice.java;
 
+import com.javapractice.java.MathProblems.MathProblems;
+
 /**
  * Hello world!
  */
 public class App {
     public static void main(String[] args) {
         System.out.println("Hello World!");
+        MathProblems mp = new MathProblems();
+        System.out.println(mp.basicArithmeticOperation1(6, 7));
+        System.out.println(mp.basicArithmeticOperation1(3, 4));
+        System.out.println(mp.basicArithmeticOperation1(5, 5));
     }
 }
---
+++ src/main/java/com/javapractice/java/MathProblems/MathProblems.java
@@ -0,0 +1,13 @@
+package com.javapractice.java.MathProblems;
+
+public class MathProblems {
+	
+	//implement multiplication of two numbers divide by two and the remainder of the result when divided by seven
+	public int basicArithmeticOperation1(int num1, int num2) {
+		int result = ((num1 * num2) / 2) % 7;
+		return result;
+	}
+	
+	//
+
+}
