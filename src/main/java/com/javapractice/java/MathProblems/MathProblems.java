3efb14662d97061d597193283aaf56baa09a4b2c
Satish Shingrakhiya <satish.shingrakhiy@gmail.com>
2025-07-10 12:55:34 +0530

    Implement Math Problem 2

--- src/main/java/com/javapractice/java/MathProblems/MathProblems.java
+++ src/main/java/com/javapractice/java/MathProblems/MathProblems.java
@@ -8,6 +8,18 @@ public class MathProblems {
 		return result;
 	}
 	
-	//
+	//Find the number as well as the sum of natural numbers, which are divisible by 2 or 7 up to a given maximum value
+	public String basicArithmeticOperation2(int num) {
+		int sum = 0, count = 0;
+		if (num >= 2) {
+			for(int i=2; i<num; i++) {
+				if (i % 2 == 0 || i % 7 == 0) {
+					count++;
+					sum+=i;
+				}
+			}
+		}
+		return "Count is: " + count + ". Sum is : " + sum + ".";
+	}
 
 }
